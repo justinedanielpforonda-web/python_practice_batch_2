b9421a5f7a3a168716d38985bae7d6822eb7ec2b
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-03-08 20:42:34 +0800

    rename problem 2

---
+++ if_the_number_isn't_equal_print_not_equal.py
@@ -0,0 +1,10 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+
+num_1 = int(input("input your first number:"))
+num_2 = int(input("input your second number:"))
+
+if num_2 != num_1:
+    print ("Not Equal")
+else:
+    print("Equal")
