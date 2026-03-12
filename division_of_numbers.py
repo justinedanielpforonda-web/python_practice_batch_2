f1910f213f098223194a40342b78e099f928c7a9
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-03-13 00:41:46 +0800

    problem 3 and 4

---
+++ division_of_numbers.py
@@ -0,0 +1,6 @@
+#  Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+num_1 = int(input("Numerator: "))
+num_2 = int(input("Denominator: "))
+
+print(num_1/num_2)
---
+++ finding_the_difference.py
@@ -0,0 +1,6 @@
+# Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+num_1= int(input("Enter your number: "))
+num_2 = int(input("Second number please: "))
+
+print (num_1 - num_2)
