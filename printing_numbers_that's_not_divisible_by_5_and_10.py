5718462f03d5b6fa62541d4b049e670d8077e568
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-03-13 00:58:37 +0800

    problems 9 and 10

---
+++ printing_numbers_that's_not_divisible_by_5_and_10.py
@@ -0,0 +1,4 @@
+# Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+for i in range (100):
+    if i % 5!= 0 and  i % 10 !=0:
+        print(i)
